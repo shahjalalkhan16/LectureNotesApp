00b00b053d76295c32aa88b61137c07c3fe8e436
Mohammad Shahjalal Khan <shahjalalkhanrabbi@gmail.com>
2023-08-21 18:17:00 +0600

    updated with Camera and store jpg as a path

--- app/src/main/java/com/example/lecturenotesapp/MainActivity.java
+++ app/src/main/java/com/example/lecturenotesapp/MainActivity.java
@@ -4,25 +4,39 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter<String> arrayAdapter;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 101;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,8 +112,68 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
-                startActivity(intent);
+                startActivity(intent); // Start the new note editor activity
+            }
+        });
+
+        Button takePictureButton = findViewById(R.id.takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
             }
         });
     }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+                // Save the image as a file and get its file path
+                String imagePath = saveImageToInternalStorage(imageBitmap);
+
+                // Create a note with the image's file path
+                String imageNote = "Image: " + imagePath;
+                String currentDate = DateFormat.getDateTimeInstance().format(new Date());
+                String combinedNote = imageNote + "\n\nLast Edited: " + currentDate;
+                notes.add(combinedNote);
+                arrayAdapter.notifyDataSetChanged();
+
+                // Store the updated notes in SharedPreferences
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+        }
+    }
+
+    private String saveImageToInternalStorage(Bitmap imageBitmap) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+
+       // File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getApplicationContext().getFilesDir();
+        File imageFile = new File(storageDir, imageFileName);
+
+        try (FileOutputStream outStream = new FileOutputStream(imageFile)) {
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            outStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return imageFile.getAbsolutePath();
+    }
+
 }
